b39860b0ecf476cca4b1173e3de06a0cce1f4db3
Mohamad Mahdi <mohmahdi110.work@gmail.com>
2024-08-08 00:18:52 +0330

    first commit to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCur, setFromCur] = useState("EUR");
+  const [toCur, setToCur] = useState("USD");
+  const [converted, setconverted] = useState("");
+  const [Loading, setIsLoading] = useState(false);
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const host = "api.frankfurter.app";
+        const res = await fetch(
+          `https://${host}/latest?amount=${amount}&from=${fromCur}&${toCur}`
+        );
+        const data = await res.json();
+        setconverted(data.rates[toCur]);
+        setIsLoading(false);
+      }
+
+      if (fromCur === toCur) return setconverted(amount);
+      convert();
+    },
+    [fromCur, toCur, amount]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="enter a Number..."
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={Loading}
+      />
+      <select
+        value={fromCur}
+        onChange={(e) => setFromCur(e.target.value)}
+        disabled={Loading}
+      >
+        {/* first part */}
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      {/* second part */}
+      <select
+        value={toCur}
+        onChange={(e) => setToCur(e.target.value)}
+        disabled={Loading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>
+        {converted}
+        {toCur}
+      </p>
     </div>
   );
 }
